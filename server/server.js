ca485cfa134a3ad35ef7a3705a906919c7c9fff4
Seth Sutherland <cokolatepal@gmail.com>
2018-01-18 14:19:50 -0700

    got basic server set up and working

--- server/server.js
+++ server/server.js
@@ -1,3 +1,22 @@
 /**
  * Created by Seth on 1/10/2018.
  */
+// EXTERNAL MODULES //
+var express = require('express');
+var bodyParser = require('body-parser');
+
+// CONFIG //
+var config = require('../config');
+
+// EXPRESS //
+var app = module.exports = express();
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + './../public'));
+
+
+
+// CONNECTIONS //
+app.listen(3051, function() {
+    console.log('Listening on port thirty fifty one...');
+});
